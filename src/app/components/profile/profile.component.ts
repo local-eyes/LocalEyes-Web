73da550d2f6cd0a0cf4679a2d630c97f97b052b1
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-07-16 02:16:45 +0530

    update profile styling

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -11,7 +11,9 @@ export class ProfileComponent implements OnInit {
 uid: string;
 personData: any;
 dataLoaded: boolean = false
-private routeSub: any;
+localFeed: any;
+cityFeed: any;
+answers: any;
   constructor(public route: ActivatedRoute, private data: DataService) { }
 
   ngOnInit(): void {
@@ -23,7 +25,50 @@ private routeSub: any;
     this.data.getProfile(uid).subscribe(res => {
       this.personData = res;
       this.dataLoaded = true;
+      console.log(this.personData);
     });
+  }
+
+  getDate(seconds:number) {
+    const months = {
+      0: "January", 
+      1: "February", 
+      2: "March", 
+      3: "April",
+      4: "May", 
+      5: "June", 
+      6: "July",
+      7: "August", 
+      8: "September", 
+      9: "October", 
+      10: "November", 
+      11: "December"
+    }
+    const postTime = new Date(seconds * 1000);
+    return `${months[postTime.getMonth()]} ${postTime.getFullYear()}`;
+  }
+
+  loadData(tabIndex:number, tabName:string) {
+    
+  }
+
+  getLocalFeed(uid: string) {
+    this.data.getUserLocalFeed(uid).subscribe(feed => {
+      this.localFeed = feed;
+      console.log(this.localFeed);
+    })
+  }
+  getCityFeed(uid: string) {
+    this.data.getUserCityFeed(uid).subscribe(feed => {
+      this.cityFeed = feed;
+      console.log(this.cityFeed);
+    })
+  }
+  getUserAnswersFeed(uid: string) {
+    this.data.getAnswersFeed(uid).subscribe(feed => {
+      this.localFeed = feed;
+      console.log(this.localFeed);
+    })
   }
 
 }
