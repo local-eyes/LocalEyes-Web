c21f6d204c0ab320e1925db270c2a595d06d6c6b
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-07-27 13:31:46 +0530

    Notifications Styled and fully functional + realtime

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
@@ -11,16 +12,46 @@ import { DataService } from 'src/app/services/data/data.service';
 export class ExploreComponent implements OnInit {
 isMobile = localStorage.getItem('isMobile');
 notifs$ : Observable<any>;
-uid = this.auth.userData.uid || null;
-  constructor(private data: DataService, private auth: AuthService) { }
+title: string = "Notifications";
+  constructor(private data: DataService, private auth: AuthService, public _location: Location) { }
 
   ngOnInit(): void {
-    if (this.uid) {
-      this.data.getNotifications(this.uid).subscribe(notif => {
+    this.auth.user$.subscribe(user => {
+      this.data.getNotifications(user.uid).subscribe(notif => {
         this.notifs$ = notif;
         console.log(this.notifs$);
       });
-    }
+    });
+  }
+
+  getDate(seconds:number) {
+    const postTime = new Date(seconds * 1000);
+    const postedOn = postTime.getTime();
+    const curr = new Date().getTime();
+    const gap = curr - postedOn;
+
+    const sec = 1000;
+    const min = sec * 60;
+    const hour = min * 60;
+    const day = hour * 24;
+
+    const fullDay = Math.floor(gap / day);
+    const fullHour = Math.floor((gap % day) / hour);
+    const fullMin = Math.floor((gap % hour) / min);
+
+    if (fullDay >= 1) {
+      return `${(postTime.toString()).slice(0, -40)}`
+    } else if (fullDay <= 0 && fullHour > 0) {
+      return `${fullHour} Hours ago`
+    } else if (fullHour <= 0 && fullMin > 0) {
+      return `${fullMin} Minutes ago`
+    } else {
+      return `> 1 Minute ago`;
+    } 
+  }
+
+  goBack() {
+    return this._location.back();
   }
 
 }
