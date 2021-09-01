755258787c27c06e9f7397c6850db8172b8b1a42
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-09-02 00:40:30 +0530

    'not open in city' component initialized

---
+++ src/app/components/not-open/not-open.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-open',
+  templateUrl: './not-open.component.html',
+  styleUrls: ['./not-open.component.css']
+})
+export class NotOpenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
